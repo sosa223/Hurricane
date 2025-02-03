0019a73615b9b6c0fe1685fc4b789bfca96a47e2
Chief <75649719+sosa223@users.noreply.github.com>
2025-02-02 21:58:24 -0500

    main.c

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+int main() {
+    int windSpeed;
+
+    printf("Enter the wind speed: ");
+    scanf("%d", &windSpeed);
+
+    if (windSpeed < 74) {
+        printf("Not a hurricane\n");
+    }
+    else if (windSpeed < 96) {
+        printf("Category 1 Hurricane\n");
+   
+    } else if (windSpeed < 111) {
+        printf("Category 2 Hurricane\n");
+   
+    } else if (windSpeed < 130) {
+        printf("Category 3 Hurricane\n");
+   
+    } else if (windSpeed < 157) {
+        printf("Category 4 Hurricane\n");
+   
+    
+    } else {
+        printf("Category 5 Hurricane\n");
+    }
+
+    return 0;
 }
